a0712ac465f2831d3012a42daa4bfc2b4a25a080
goatpretty <h280791@gmail.com>
2025-10-06 14:40:26 +0800

    自动更新：2025/10/06 周一 14:40:26.89

--- src/data/moreLinks.ts
+++ src/data/moreLinks.ts
@@ -1,6 +1,12 @@
-export default [
-  {
-    name: "My OpenList",
-    url: "https://list.goatpretty.com",
-  },
+// src/data/moreLinks.ts
+
+export interface MoreLink {
+  name: string;
+  url: string;
+}
+
+const moreLinks: MoreLink[] = [
+  { name: 'My OpenList', url: 'https://list.goatpretty.com' },
 ];
+
+export default moreLinks;
